436cee915ee5f0b7857f3508a537cdaa00063e94
sothbak <61996130+sothbak@users.noreply.github.com>
2021-02-14 02:17:54 +1100

    slightly optimised

--- buger-builder/src/components/Burger/OrderSummary/OrderSummary.js
+++ buger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,30 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../../hoc/Auxil';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
-        .map(igKey => {
-            return (
-                <li key={igKey}>
-                    <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
-                </li>);
-        });
+class OrderSummary extends Component {
+    componentDidUpdate() {
+        console.log("[OrderSummar] updates");
+    };
 
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>Your burger has the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Your total cost is: ${props.price.toFixed(2)}</strong></p>
-            <p>Continue to checkout?</p>
-            <Button buttonType='Danger' clicked={props.purchaseCancel}>CANCEL</Button>
-            <Button buttonType='Success' clicked={props.purchaseContinue}>CONTINUE</Button>
-        </Aux>
-    )
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients)
+            .map(igKey => {
+                return (
+                    <li key={igKey}>
+                        <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
+                    </li>);
+            });
+        
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>Your burger has the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Your total cost is: ${this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to checkout?</p>
+                <Button buttonType='Danger' clicked={this.props.purchaseCancel}>CANCEL</Button>
+                <Button buttonType='Success' clicked={this.props.purchaseContinue}>CONTINUE</Button>
+            </Aux>
+        );
+    };
 };
 
-export default orderSummary;
+export default OrderSummary;
